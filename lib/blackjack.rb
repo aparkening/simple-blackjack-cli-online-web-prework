3e11be4d6c02c02bd9c33a74bee1d07f239835b5
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-20 17:04:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,18 +34,9 @@ def initial_round
   second = deal_card
   sum = first + second
   display_card_total(sum)
-  
-    expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
-    expect(initial_round).to eq(12)
-  end
-
-  it "calls on the '#display_card_total' to print sum of cards" do
-    expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
-    expect($stdout).to receive(:puts).with(/Your cards add up to /)
-    initial_round
-  end
 end
 
+# 
 def hit?
   # code hit? here
 end
